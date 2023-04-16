b7d1d7a3e98cbae5304fd7d221a547848dfdba83
rockrick1 <henriquecerquinho@gmail.com>
2023-04-16 17:20:03 -0300

    feat: improve combo animation and position

--- Assets/Scripts/MVC/Views/Game/ComboView.cs
+++ Assets/Scripts/MVC/Views/Game/ComboView.cs
@@ -1,18 +1,25 @@
-﻿using TMPro;
+﻿using DG.Tweening;
+using TMPro;
 using UnityEngine;
 
 public class ComboView : MonoBehaviour
 {
     const string COMBO_FORMAT = "{0}x";
-        
-    [SerializeField] Animator animator;
-    [SerializeField] TextMeshProUGUI text;
     
-    static readonly int UpdateCombo = Animator.StringToHash("comboChanged");
+    [SerializeField] TextMeshProUGUI text;
+    [SerializeField] float animDuration;
+    
+    readonly Vector3 scale = new(1.3f, 1.2f, 1f);
+    readonly Vector3 original = new(1f, 1f, 1f);
+
+    Sequence sequence;
 
     public void SetCombo (int combo)
     {
+        sequence = DOTween.Sequence();
+        sequence.Append(text.transform.DOScale(scale, animDuration / 2));
+        sequence.Append(text.transform.DOScale(original, animDuration / 2));
+        sequence.Play();
         text.text = string.Format(COMBO_FORMAT, combo);
-        animator.SetTrigger(UpdateCombo);
     }
 }
